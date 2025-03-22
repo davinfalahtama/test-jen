5309efe33828b8c82b0ed8b8365558a12cef2513
davin <davinfalahtama@gmail.com>
2025-03-22 11:54:52 +0700

    Refactor Jenkinsfiles to ensure correct directory context for script execution

--- pipeline-1/model.py
+++ pipeline-1/model.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import joblib
+# import joblib
 
 # Load dataset
 data = pd.read_csv('pipeline-1\data.csv')
